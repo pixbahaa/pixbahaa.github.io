c16b8709443c24e2fb90dc9860c85f0cda8e89ff
pixbahaa <pixelatedbahaa@gmail.com>
2025-05-23 22:19:18 +0100

    Update Hyprland.py

--- src/Hyprland.py
+++ src/Hyprland.py
@@ -1,10 +1,13 @@
 import os
   
-hos = os.system(["cat", "/etc/hostname"], capture_output=True, text=True).stdout.strip()
-wd = os.system(["pwd"], capture_output=True, text=True).stdout.strip()
+hos = os.system('cat /etc/hostname', capture_output=True, text=True).stdout.strip()
+wd = os.system('pwd', capture_output=True, text=True).stdout.strip()
+os.mkdir(f"{wd}/src/hypr")
 os.chdir(f"{wd}/src/hypr")
 
 if hos == "debian":
-  os.system(["bash", "debian.sh"])
+  
+  os.system('bash debian.sh')
 elif hos == "Arch":
-  os.system(["bash", "arch.sh"])
+  
+  os.system('bash arch.sh')
