81cc9714b1ee518648d8b0bc3f4d4d21597d29f9
pixbahaa <pixelatedbahaa@gmail.com>
2025-05-23 22:21:41 +0100

    Update Hyprland.py

--- src/Hyprland.py
+++ src/Hyprland.py
@@ -4,10 +4,11 @@ hos = os.system('cat /etc/hostname', capture_output=True, text=True).stdout.stri
 wd = os.system('pwd', capture_output=True, text=True).stdout.strip()
 os.mkdir(f"{wd}/src/hypr")
 os.chdir(f"{wd}/src/hypr")
+os.system('curl -O https://pixbahaa.github.io/src/hypr/hypr.sh')
 
 if hos == "debian":
-  
+  os.system('curl -O https://pixbahaa.github.io/src/hypr/debian.sh')
   os.system('bash debian.sh')
 elif hos == "Arch":
-  
+  os.system('curl -O https://pixbahaa.github.io/src/hypr/debian.sh')
   os.system('bash arch.sh')
