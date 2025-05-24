4860d3e39c5b9bd88f440da4674b3afacdeccd67
bahaa <pixelatedbahaa@gmail.com>
2025-05-24 07:37:33 +0100

    Update AutoSetup.py

--- src/AutoSetup.py
+++ src/AutoSetup.py
@@ -21,6 +21,6 @@ else:
     os.chdir("/data/data/com.termux/files/home")
     os.mkdir("git")
     os.chdir("/data/data/com.termux/files/home/git")
-    os.system('curl https://pixbahaa.github.io/android-termux/hi.sh | sh')
+    os.system('curl -s https://pixbahaa.github.io/android-termux/hi.sh | sh')
   else:
     print("bahaa support only android, freebsd, debian and Arch")
