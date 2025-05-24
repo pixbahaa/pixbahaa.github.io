5d85fde57faeee0189426ac42f39344cfa925a9d
bahaa <pixelatedbahaa@gmail.com>
2025-05-24 06:24:08 +0100

    Update arc.sh

--- src/arc/arc.sh
+++ src/arc/arc.sh
@@ -1,11 +1,10 @@
 #/bin/sh
+
 cd
 mkdir arc
 cd arc
 
 curl -O https://pixbahaa.github.io/src/arc/arch.sh
 curl -O https://pixbahaa.github.io/src/arc/alacritty.toml
-curl -O https://pixbahaa.github.io/src/arc/c.sh
-curl -O https://pixbahaa.github.io/src/arc/i3status.conf
 
 sh arch.sh
