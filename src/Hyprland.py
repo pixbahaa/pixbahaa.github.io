dc381ac1e9be91f916ac89288e955433112d544a
bahaa <pixelatedbahaa@gmail.com>
2025-05-24 09:11:30 +0100

    Update Hyprland.py

--- src/Hyprland.py
+++ src/Hyprland.py
@@ -9,7 +9,13 @@ os.system('curl -O https://pixbahaa.github.io/src/hypr/hypr.sh')
 
 if hos == "debian":
   os.system('curl -O https://pixbahaa.github.io/src/hypr/debian.sh')
+  os.system('chmod +x debian.sh')
   os.system('bash debian.sh')
 elif hos == "Arch":
   os.system('curl -O https://pixbahaa.github.io/src/hypr/debian.sh')
+  os.system('chmod +x arch.sh')
   os.system('bash arch.sh')
+if platform.system() == "FreeBSD":
+  os.system('curl -O https://pixbahaa.github.io/src/hypr/freebsd.sh')
+  os.system('chmod +x freebsd.sh')
+  os.system('sh freebsd.sh')  
