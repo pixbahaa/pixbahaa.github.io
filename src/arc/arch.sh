c590b568aac41212a07c6fc8a787407306e4748c
bahaa <pixelatedbahaa@gmail.com>
2025-05-24 06:22:34 +0100

    Update arch.sh

--- src/arc/arch.sh
+++ src/arc/arch.sh
@@ -2,23 +2,18 @@
 
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm noto-fonts noto-fonts-emoji noto-fonts-extra ttf-noto-nerd
-sudo pacman -S --noconfirm xorg xorg-drivers xorg-xinit i3 alacritty dmenu base-devel grml-zsh-config powerline python-click-completion zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting intel-ucode pulseaudio pavucontrol
+sudo pacman -S --noconfirm xorg xorg-drivers xorg-xinit dwm alacritty dmenu base-devel zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting intel-ucode pulseaudio pavucontrol
 sudo usermod -s /bin/zsh bahaa
 echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> /home/bahaa/.zshrc
 echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> /home/bahaa/.zshrc
 echo "source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh" >> /home/bahaa/.zshrc
 
-
 git clone https://aur.archlinux.org/paru-bin
 cd paru-bin
 makepkg -si --noconfirm
 cd ..
+rm -rf paru-bin
 
 mkdir -p /home/bahaa/.config/alacritty
-cp alacritty.toml /home/bahaa/.config/alacritty/
-cp -r ../../walls /home/bahaa/
-echo "exec i3" > /home/bahaa/.xinitrc
-mkdir /home/bahaa/.config/i3status
-cp i3status.config /home/bahaa/.config/i3status/config
-
-echo "reboot and run i3 for the fist time to auto generate config and then run c.sh"
+cp alacritty.toml .config/alacritty/
+echo "exec dwm" > .xinitrc
