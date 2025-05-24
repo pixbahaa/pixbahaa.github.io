4506ab8e0c1f0b269eb1528510c039b9ebb98684
bahaa <pixelatedbahaa@gmail.com>
2025-05-24 06:16:45 +0100

    Update debian.sh

--- src/deb/debian.sh
+++ src/deb/debian.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
+cd
 
 sudo apt update && sudo apt full-upgrade -y
 sudo apt install fonts-noto-core fonts-noto-mono fonts-noto-color-emoji
-sudo apt install xorg i3 feh alacritty pulseaudio pavucontrol tldr network-manager w3m unzip thunar thunar-volman neovim -y
+sudo apt install xorg dwm suckless-tools feh alacritty pulseaudio pavucontrol tldr w3m unzip thunar thunar-volman neovim -y
 
-mkdir -p .config/alacritty .config/i3status
+mkdir -p .config/alacritty
 
-cp i3status.conf /home/bahaa/.config/i3status/config
-cp alacritty.yml /home/bahaa/.config/alacritty/
-echo "exec i3" > /home/bahaa/.xinitrc
-cp -r ../../walls /home/bahaa/
+cp alacritty.yml .config/alacritty/
+echo "exec i3" > .xinitrc
+
+cd /tmp
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
 sudo apt install -f -y
-
-sudo rm /etc/network/interfaces
-sudo systemctl enable NetworkManager
-
-echo "reboot and run i3 for the fist time to auto generate config"
-echo "and then use nmtui and also comment out the last line of i3 config in bar section"
-echo "and finally run c.sh"
